ac717e5e76e8db968d02d83019a4d609bf695de2
Dennis Ploetner <re@lloc.de>
2025-09-09 14:13:19 +0200

    Stupid update

--- casa-vacanze-miro.php
+++ casa-vacanze-miro.php
@@ -4,10 +4,10 @@
 * Plugin Name: Custom plugin casa-vacanze-miro.com
 * Plugin URI:  https://github.com/lloc/casa-vancanze-miro
 * Description: Sets some custom stuff.
-* Version:     1.0.0
+* Version:     1.0.1
 * Network:     true
 */
 
 add_filter( 'global_media.site_id', function() {
-   return 10;
+   return 6;
 } );
